6494e46eaa95222f2c348ca9db5e26bbbeef68cd
Steve Gill <stevengill@slack-corp.com>
2020-09-08 10:22:17 -0700

    initial push

---
+++ README.md
@@ -0,0 +1,56 @@
+# Frontiers 2020 Developer Tools Demo App
+
+This is a quick example app showing off some of the functionality available in bolt-js. 
+
+Local development requires a public URL where Slack can send requests. In this guide, we'll be using [`ngrok`](https://ngrok.com/download). Checkout [this guide](https://api.slack.com/tutorials/tunneling-with-ngrok) for setting it up.
+
+Before we get started, make sure you have a development workspace where you have permissions to install apps. If you don’t have one setup, go ahead and [create one](https://slack.com/create). You also need to [create a new app](https://api.slack.com/apps?new_app=1) if you haven’t already.
+
+## Install Dependencies
+
+```
+npm install
+```
+
+## Install app to workspace
+
+In your [**App Config** Page](https://api.slack.com/apps), go to **OAuth & Permissions** and add the `channels:read`, `app_mentions:read`, and `chat:write` permissions. Click **install App** to install the app to your workspace and generate a bot token.
+
+## Setup Environment Variables
+
+This app requires you setup a few environment variables. You can get these values by navigating to your [**App Config** Page](https://api.slack.com/apps). 
+
+```
+export SLACK_BOT_TOKEN=YOUR_SLACK_CLIENT_SECRET
+export SLACK_SIGNING_SECRET=YOUR_SLACK_SIGNING_SECRET
+```
+
+## Run the App
+
+Start the app with the following command:
+
+```
+npm start
+```
+
+This will start the app on port `3000`.
+
+Now lets start `ngrok` so we can access the app on an external network and create a `redirect url` for OAuth. 
+
+```
+ngrok http 3000
+```
+
+This should output a forwarding address for `http` and `https`. Take note of the `https` one. It should look something like the following:
+
+```
+Forwarding   https://3cb89939.ngrok.io -> http://localhost:3000
+```
+
+Navigate to the **App Home** page in your app config and enable it. Then navigate to **Event Subscriptions** to enable it. The request URL should be set to your `ngrok` forwarding address with the `slack/events` path appended. ex:
+
+```
+https://3cb89939.ngrok.io/slack/events
+```
+
+Lastly, in the **Events Subscription** page, click **Subscribe to bot events** and add `app_home_opened`, `app_mentioned`, and `message.channels`.  
---
+++ app.js
@@ -0,0 +1,167 @@
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
+
+// subscribe to 'app_mention' event in your App config
+// need app_mentions:read and chat:write scopes
+// app.event('app_mention', async ({ event, context, client }) => {
+//   try {
+//     // directly call the api method 'chat.postMessage'
+//     const result = await client.chat.postMessage({
+//       token: context.botToken,
+//       channel: event.channel,
+//       text: `Thanks for the mention, <@${event.user}>!`
+//     });
+//   }
+//   catch (error) {
+//     console.error(error);
+//   }
+// });
+
+// subscribe to `message.channels` event in your App Config
+// need channels:read scope
+// app.message('hello', async ({ message, say }) => {
+//   console.log('app.message');
+//   console.log(message);
+//   // say() sends a message to the channel where the event was triggered
+//   // no need to directly use 'chat.postMessage', no need to include token
+//   await say({"blocks": [
+// 		{
+// 			"type": "section",
+// 			"text": {
+// 				"type": "mrkdwn",
+// 				"text": `Thanks for the mention <@${message.user}>! Click my fancy button`
+// 			},
+// 			"accessory": {
+// 				"type": "button",
+// 				"text": {
+// 					"type": "plain_text",
+// 					"text": "Button",
+// 					"emoji": true
+// 				},
+//         "value": "click_me_123",
+//         "action_id": "first_button"
+// 			}
+// 		}
+// 	]});
+// });
+
+// Listen and respond to button click
+// app.action('first_button', async({action, ack, say, context}) => {
+//   console.log('button clicked');
+//   console.log(action);
+//   // acknowledge the request right away
+//   await ack();
+//   await say('Thanks for clicking the fancy button');
+// });
+
+// app home demo
+// app.event('app_home_opened', async ({ event, context, client }) => {
+//   console.log('app home opened');
+//   console.log(event.tab);
+//   console.log(event.user);
+//   try {
+//     if (event.tab === 'home') {
+//       const result = await client.views.publish({
+//         token: context.botToken,
+//         user_id: event.user,
+//         view: { 
+//           "type":"home",
+//           "blocks":[ 
+//             { 
+//               "type":"section",
+//               "text":{ 
+//                 "type":"mrkdwn",
+//                 "text":"A simple stack of blocks for the simple sample Block Kit Home tab."
+//               }
+//             },
+//             { 
+//               "type":"actions",
+//               "elements":[ 
+//                 { 
+//                   "type":"button",
+//                   "text":{ 
+//                     "type":"plain_text",
+//                     "text":"Action A",
+//                     "emoji":true
+//                   }
+//                 },
+//                 { 
+//                   "type":"button",
+//                   "text":{ 
+//                     "type":"plain_text",
+//                     "text":"Action B",
+//                     "emoji":true
+//                   }
+//                 }
+//               ]
+//             }
+//           ]
+//        },
+//       });
+//       console.log(result);
+//     }
+//   }
+//   catch (error) {
+//     console.error(error);
+//   }
+// });
+
+// setup shortcut in your App config page
+// app.shortcut('launch_shortcut', async ({ shortcut, ack, context, client }) => {
+
+//   try {
+//     // Acknowledge shortcut request
+//     await ack();
+
+//     // Call the views.open method using one of the built-in WebClients
+//     const result = await client.views.open({
+//       // The token you used to initialize your app is stored in the `context` object
+//       token: context.botToken,
+//       trigger_id: shortcut.trigger_id,
+//       view: {
+//         type: "modal",
+//         title: {
+//           type: "plain_text",
+//           text: "My App"
+//         },
+//         close: {
+//           type: "plain_text",
+//           text: "Close"
+//         },
+//         blocks: [
+//           {
+//             type: "section",
+//             text: {
+//               type: "mrkdwn",
+//               text: "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+//             }
+//           },
+//           {
+//             type: "context",
+//             elements: [
+//               {
+//                 type: "mrkdwn",
+//                 text: "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+//               }
+//             ]
+//           }
+//         ]
+//       }
+//     });
+//   }
+//   catch (error) { 
+//     console.error(error);
+//   }
+// });
